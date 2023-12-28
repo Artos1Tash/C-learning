3c721e2ccee7107e8b0a5bd72b5321a1fa880a2a
Kratos1Top <strong.willed343@gmail.com>
2023-12-28 12:03:05 -0400

    Created calculator in C

--- calculator/calc.c
+++ calculator/calc.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
 
 int main(){
-    double num1, num2;
+    float num1, num2;
     char operator;
 
     printf("Enter operator, num1 and num2: ");
     // some basic input
-    scanf("%c %lf %lf", &operator, &num1, &num2);
+    scanf("%c %f %f", &operator, &num1, &num2);
 
     printf("Success!\n");
-    printf("%lf %c %lf", num1, operator, num2);
+
+    float final;
+    if (operator == '/' && num2 == 0 ){
+        printf("GO to school, you can't divide by zero!\n");
+        return 1;
+    }
+
+    switch (operator){
+        case '+':
+            final = num1 + num2;
+            break;
+        case '-':
+            final = num1 - num2;
+            break;
+        case '*':
+            final = num1 * num2;
+            break;
+        case '/':
+            final = num1 / num2;
+            break;
+        default:
+            printf("Invalid operator\n");
+            return 1;
+    }
+
+    printf("%0.2f %c %0.2f = %0.2f \n", num1, operator, num2, final);
+
     return 0;
 }
-
